7905f745db2f8ac00c3f5020433cc8b710cd6d4b
el2toro <vasea.badragan@gmail.com>
2025-09-12 09:25:59 +0200

    config  project

---
+++ src/app/features/dashboard-feature/dashboard-feature.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-feature',
+  templateUrl: './dashboard-feature.component.html',
+  styleUrls: ['./dashboard-feature.component.scss'],
+  standalone: true
+})
+export class DashboardFeatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
