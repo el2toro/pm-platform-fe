a8dd189de2c80e1516b9d4e15a4c938f54a35b59
el2toro <vasea.badragan@gmail.com>
2025-09-18 21:04:56 +0200

    include stiky nav menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from './core/components/menu/menu.component';
 import { RouterOutlet } from '@angular/router';
+import { DashboardNavComponent } from "./features/dashboard-feature/components/dashboard-nav/dashboard-nav.component";
 
 
 @Component({
     selector: 'app-root',
-    imports: [MenuComponent, RouterOutlet],
+    imports: [MenuComponent, RouterOutlet, DashboardNavComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
