3caf1f9dbe5376b0cd4df9933d670359c1853d16
el2toro <vasea.badragan@gmail.com>
2025-09-21 22:23:45 +0200

    add auth guard

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,26 @@
+// src/app/guards/auth.guard.ts
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  Router,
+  UrlTree
+} from '@angular/router';
+import { AuthService } from '../auth/services/auth.service';
+
+
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService,
+              private router: Router) {}
+
+  canActivate(): boolean | UrlTree {
+    if (this.authService.isLoggedIn()) {
+        console.log('user logged in ')
+      return true; // ✅ Allow navigation
+    }
+    // ❌ Not logged in → redirect to login page
+    console.log('user not logged in ')
+    return this.router.parseUrl('/login');
+  }
+}
