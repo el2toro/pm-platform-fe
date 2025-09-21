c7db339d9169dd2a24ec258f989b25d5b0148df9
el2toro <vasea.badragan@gmail.com>
2025-09-21 22:22:57 +0200

    add login component

--- src/app/core/auth/login/login.component.ts
+++ src/app/core/auth/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { InputText } from "primeng/inputtext";
+import { FloatLabel } from "primeng/floatlabel";
+import { ButtonDirective } from "primeng/button";
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { LoginRequestModel } from '../models/login-request.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  imports: [InputText, FloatLabel, ButtonDirective]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, 
+    private router: Router) { }
 
   ngOnInit() {
   }
 
+  login(){
+    //TODO: Add real service api call 
+    let request = new LoginRequestModel();
+    request.email = 'michael.smith@tenant123.com'
+    request.password = 'Password123'
+    this.authService.login(request);
+  
+    this.router.navigate(['/dashboard'])
+    
+  }
 }
---
+++ src/app/core/auth/models/login-response.ts
@@ -0,0 +1,8 @@
+export class LoginResponse {
+  userId = '';
+  tenantId = '';
+  email = '';
+  token = '';
+  refreshToken = '';
+  roles = [];
+}
