8354fade5b33771295e5a7fa0c73f51ad07fae7c
el2toro <vasea.badragan@gmail.com>
2025-09-25 13:31:52 +0200

    implement task details page

--- src/app/features/dashboard-feature/pages/task-details-page/task-details-page.component.ts
+++ src/app/features/dashboard-feature/pages/task-details-page/task-details-page.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskStatusPipe } from './../../pipes/task-status.pipe';
+import { CommonModule } from '@angular/common';
+import { TaskModel } from '../../models/task-model';
+import { TableModule } from "primeng/table";
 
 @Component({
   selector: 'app-task-details-page',
   templateUrl: './task-details-page.component.html',
   styleUrls: ['./task-details-page.component.scss'],
   standalone: true,
+  imports: [TaskStatusPipe, CommonModule, TableModule],
 })
 export class TaskDetailsPageComponent implements OnInit {
+  projectTitle = '';
+  projectDescription = '';
+  task = new TaskModel(); 
 
   constructor() { }
 
   ngOnInit() {
+    this.projectTitle = history.state.projectTitle;
+    this.projectDescription = history.state.projectDescription;
+    this.task = history.state.task;
   }
 
 }
