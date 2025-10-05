a5bcb3831ed7bf031d429a1dbcb9d856216d7a19
el2toro <vasea.badragan@gmail.com>
2025-10-05 15:04:24 +0200

    update task service

--- src/app/features/dashboard-feature/apis/task/task.service.ts
+++ src/app/features/dashboard-feature/apis/task/task.service.ts
@@ -10,27 +10,31 @@ import { TaskStatus } from '../../enums/task-status.enum';
 export class TaskService {
   constructor() {}
   private httpClient = inject(HttpClient);
-  private baseUrl = 'https://localhost:7186/api/tasks';
+  private baseUrl = 'https://localhost:5054/task-service';
 
   createTask(task: TaskModel): Observable<any> {
-    return this.httpClient.post<any>(this.baseUrl, task);
+    return this.httpClient.post<any>(`${this.baseUrl}/project/${task.projectId}/tasks`, task);
   }
 
   updateTask(task: TaskModel): Observable<any> {
-    return this.httpClient.put<any>(this.baseUrl, task);
+    return this.httpClient.put<any>(`${this.baseUrl}/project/${task.projectId}/tasks`, task);
   }
 
-  deleteTask(taskId: TaskModel): Observable<any> {
+  deleteTask(projectId: string,  taskId: TaskModel): Observable<any> {
     const params = new HttpHeaders();
     params.append('taskId', taskId.toString());
-    return this.httpClient.delete<any>(this.baseUrl, { headers: params });
+    return this.httpClient.delete<any>(`${this.baseUrl}/project/${projectId}/tasks`, { headers: params });
   }
 
-  updateTaskStatus(taskId: string, columnId: string, status: TaskStatus): Observable<any> {
-    const url = `${this.baseUrl}/status`;
+  updateTaskStatus(projectId: string, taskId: string, status: TaskStatus): Observable<any> {
+    const url = `${this.baseUrl}/project/${projectId}/tasks/${taskId}/status`;
     // const params = new HttpParams()
     //   .set('taskId', taskId)
     //   .set('status', status);
-    return this.httpClient.patch<any>(url, { taskId: taskId, columnId: columnId, status: status });
+    return this.httpClient.patch<any>(url, { projectId: projectId, taskId: taskId, status: status });
+  }
+
+  getTasks(projectId: string){
+    return this.httpClient.get<TaskModel[]>(`${this.baseUrl}/project/${projectId}/tasks`);
   }
 }
