ac31e0bd43b30427dfa7b64c6fb66e1adfa04219
el2toro <vasea.badragan@gmail.com>
2025-10-05 15:03:17 +0200

    add board service

---
+++ src/app/features/dashboard-feature/apis/board/board.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { BoardModel } from '../../models/board.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BoardService {
+  private http = inject(HttpClient);
+  private baseUrl = 'https://localhost:5054/board-service/project';
+
+  constructor() {}
+
+  getBoard(projectId: string, boardId: string): Observable<BoardModel> {
+    return this.http.get<BoardModel>(
+      `${this.baseUrl}/${projectId}/boards/${boardId}`
+    );
+  }
+
+  getBoards(projectId: string): Observable<BoardModel[]> {
+    return this.http.get<BoardModel[]>(`${this.baseUrl}/${projectId}/boards`);
+  }
+
+  createBoard(board: BoardModel): Observable<BoardModel> {
+    return this.http.post<BoardModel>(
+      `${this.baseUrl}/${board.projectId}/boards`,
+      board
+    );
+  }
+
+  updateBoard(board: BoardModel): Observable<BoardModel> {
+    return this.http.put<BoardModel>(
+      `${this.baseUrl}/${board.projectId}/boards`,
+      board
+    );
+  }
+
+  deleteBoard(projectId: string, boardId: string): Observable<any> {
+    return this.http.delete<any>(
+      `${this.baseUrl}/${projectId}/boards/${boardId}`
+    );
+  }
+}
