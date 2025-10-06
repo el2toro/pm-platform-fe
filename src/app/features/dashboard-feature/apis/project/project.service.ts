ab0806f15a4c4c240105b58420b57559c435171d
el2toro <vasea.badragan@gmail.com>
2025-10-06 13:20:23 +0200

    update project service

--- src/app/features/dashboard-feature/apis/project/project.service.ts
+++ src/app/features/dashboard-feature/apis/project/project.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { ProjectModel } from '../../models/project-model';
 import { BoardModel } from '../../models/board.model';
 
@@ -11,7 +11,23 @@ export class ProjectService {
   private http = inject(HttpClient);
   private baseUrl = 'https://localhost:5054/project-service/';
 
+  private projectsSubject = new BehaviorSubject<ProjectModel[]>([]);
+  public projects$ = this.projectsSubject.asObservable();
+
   constructor() {}
+
+  get currentProjects(): ProjectModel[] {
+    return this.projectsSubject.value;
+  }
+
+  addProject(newProject: ProjectModel): void {
+    const updated = [...this.projectsSubject.value, newProject];
+    this.projectsSubject.next(updated);
+  }
+
+  setProjects(projects: ProjectModel[]): void {
+    this.projectsSubject.next(projects);
+  }
 
   getProjects(pageNumber: number, pageSize: number): Observable<any> {
     let params = new HttpParams();
