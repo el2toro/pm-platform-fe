8af21deace12fe23ce1ef46fc8247b2efc85ed60
el2toro <vasea.badragan@gmail.com>
2025-10-07 07:11:27 +0200

    update signalr service

--- src/shared/services/signalR/signalR.service.ts
+++ src/shared/services/signalR/signalR.service.ts
@@ -2,13 +2,17 @@ import { inject, Injectable, NgZone } from '@angular/core';
 import * as signalR from '@microsoft/signalr';
 import { ProjectModel } from '../../../app/features/dashboard-feature/models/project-model';
 import { ProjectService } from '../../../app/features/dashboard-feature/apis/project/project.service';
+import { registerTaskHandlers } from './taskHandlers/task-handlers';
+import { TaskService } from '../../../app/features/dashboard-feature/apis/task/task.service';
+import { registerProjectHandlers } from './projectHandlers/project-handlers';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SignalRService {
   private hubConnection!: signalR.HubConnection;
-  private baseUrl = 'https://localhost:5054/project-service/hub/project';
+  private baseUrl = 'https://localhost:5054/task-service/hub/notifications';
+  private taskService = inject(TaskService);
 
 constructor() { }
 private projectService = inject(ProjectService);
@@ -17,6 +21,9 @@ private zone = inject(NgZone)
  startConnection(): void {
     this.hubConnection = new signalR.HubConnectionBuilder()
       .withUrl(this.baseUrl)
+//       .withUrl(`${apiUrl}/hubs/project`, {
+//   accessTokenFactory: () => this.authService.getAccessToken()
+// })
       .withAutomaticReconnect()
       .build();
 
@@ -25,19 +32,30 @@ private zone = inject(NgZone)
       .then(() => console.log('SignalR Connected ✅'))
       .catch(err => console.error('SignalR Connection Error ❌', err));
 
-    this.hubConnection.on('ReceiveProjects', (response: ProjectModel) => {
-      this.zone.run(() => {
-        this.projectService.addProject(response);
-        console.log('Projects received:', response);
-      })
-    });
+      this.hubConnection.on('ReceiveCreatedTask', (createdTask) => {
 
-    this.hubConnection.on('ReceiveUpdatedProject', (response: ProjectModel) => {
-      this.zone.run(() => {
-       // this.projectService.addProject(response);
-        console.log('Projects received:', response);
-      })
-    });
+        this.zone.run(() => {
+        this.taskService.addTask(createdTask)
+        console.log('Task  received:', createdTask)
+      })});
+     
+
+     // registerTaskHandlers(this.hubConnection, this.taskService);
+      registerProjectHandlers(this.hubConnection, this.projectService)
+
+    // this.hubConnection.on('ReceiveProjects', (response: ProjectModel) => {
+    //   this.zone.run(() => {
+    //     this.projectService.addProject(response);
+    //     console.log('Projects received:', response);
+    //   })
+    // });
+
+    // this.hubConnection.on('ReceiveUpdatedProject', (response: ProjectModel) => {
+    //   this.zone.run(() => {
+    //    // this.projectService.addProject(response);
+    //     console.log('Projects received:', response);
+    //   })
+    // });
     }
 
 }
