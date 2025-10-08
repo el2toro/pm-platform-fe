8db6777f65002cd4ab809ea44efd473cb507946b
el2toro <vasea.badragan@gmail.com>
2025-10-08 16:10:17 +0200

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { AuthService } from './core/auth/services/auth.service';
-import { MenuComponent } from "./core/components/menu/menu.component";
-import { DashboardNavComponent } from "./features/dashboard-feature/components/dashboard-nav/dashboard-nav.component";
 import { SignalRService } from '../shared/services/signalR/signalR.service';
+import { ShellComponent } from "./core/shell/shell.component";
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, MenuComponent, DashboardNavComponent],
+    imports: [ShellComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 
 export class AppComponent implements OnInit{
   title = 'pm-platform-fe';
-  private authService = inject(AuthService);
   private signalRService = inject(SignalRService);
-
-  get userIsLoggedIn(): boolean{
-    return this.authService.isLoggedIn();
-  }
 
   constructor() { }
 
