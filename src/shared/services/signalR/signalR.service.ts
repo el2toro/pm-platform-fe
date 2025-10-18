8da271b03c6a83946b2fc3379d66b821ddfbe068
el2toro <vasea.badragan@gmail.com>
2025-10-18 18:15:26 +0200

    add jwt to signalr requests

--- src/shared/services/signalR/signalR.service.ts
+++ src/shared/services/signalR/signalR.service.ts
@@ -5,50 +5,34 @@ import { ProjectService } from '../../../app/features/dashboard-feature/apis/pro
 import { registerTaskHandlers } from './taskHandlers/task-handlers';
 import { TaskService } from '../../../app/features/dashboard-feature/apis/task/task.service';
 import { registerProjectHandlers } from './projectHandlers/project-handlers';
+import { AuthService } from '../../../app/core/auth/services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SignalRService {
   private hubConnection!: signalR.HubConnection;
   private baseUrl = 'https://localhost:5054/task-service/hub/notifications';
   private taskService = inject(TaskService);
+  private authService = inject(AuthService);
 
-constructor() { }
-private projectService = inject(ProjectService);
-private zone = inject(NgZone)
+  constructor() {}
+  private projectService = inject(ProjectService);
 
- startConnection(): void {
+  startConnection(): void {
     this.hubConnection = new signalR.HubConnectionBuilder()
-      .withUrl(this.baseUrl)
-//       .withUrl(`${apiUrl}/hubs/project`, {
-//   accessTokenFactory: () => this.authService.getAccessToken()
-// })
+      .withUrl(this.baseUrl, {
+        accessTokenFactory: () => this.authService.accessToken as string,
+      })
       .withAutomaticReconnect()
       .build();
 
-        this.hubConnection
+    this.hubConnection
       .start()
       .then(() => console.log('SignalR Connected ✅'))
-      .catch(err => console.error('SignalR Connection Error ❌', err));
-     
+      .catch((err) => console.error('SignalR Connection Error ❌', err));
 
-      registerTaskHandlers(this.hubConnection, this.taskService);
-      registerProjectHandlers(this.hubConnection, this.projectService)
-
-    // this.hubConnection.on('ReceiveProjects', (response: ProjectModel) => {
-    //   this.zone.run(() => {
-    //     this.projectService.addProject(response);
-    //     console.log('Projects received:', response);
-    //   })
-    // });
-
-    // this.hubConnection.on('ReceiveUpdatedProject', (response: ProjectModel) => {
-    //   this.zone.run(() => {
-    //    // this.projectService.addProject(response);
-    //     console.log('Projects received:', response);
-    //   })
-    // });
-    }
-
+    registerTaskHandlers(this.hubConnection, this.taskService);
+    registerProjectHandlers(this.hubConnection, this.projectService);
+  }
 }
