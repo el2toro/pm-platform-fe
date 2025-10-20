65f563ac543894fa48a6fb7767af707881872f49
el2toro <vasea.badragan@gmail.com>
2025-10-20 20:26:48 +0200

    update user profile

--- src/app/features/profile-feature/profile-feature.component.ts
+++ src/app/features/profile-feature/profile-feature.component.ts
@@ -4,6 +4,9 @@ import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 import { FileUploadModule } from 'primeng/fileupload';
+import { Toast } from "primeng/toast";
+import { CustomMessageService } from '../../../shared/services/custom-message.service';
+import { Router } from '@angular/router';
 
 interface Profile{
   firstName: string;
@@ -12,18 +15,23 @@ interface Profile{
   phoneNumber: string;
   aboutYou: string;
   image: string;
+  birthDate: string;
+  address: string;
 }
 
 @Component({
   selector: 'app-profile-feature',
   templateUrl: './profile-feature.component.html',
   styleUrls: ['./profile-feature.component.scss'],
-  imports: [ButtonModule, InputTextModule, FloatLabelModule, FormsModule, ReactiveFormsModule, FileUploadModule]
+  imports: [ButtonModule, InputTextModule, FloatLabelModule, FormsModule, ReactiveFormsModule, FileUploadModule, Toast]
 })
 export class ProfileFeatureComponent implements OnInit {
   private formBuilder = inject(FormBuilder);
+  private customMessageService = inject(CustomMessageService);
+  private router = inject(Router);
   form!: FormGroup;
   profile!: Profile;
+  buttonDisabled = true;
 
   constructor() { }
 
@@ -34,7 +42,9 @@ export class ProfileFeatureComponent implements OnInit {
       email: 'eugenia.torry@devcon.com', 
       phoneNumber: '+44 365 459 894', 
       aboutYou: 'I am a greate one!',
-      image: 'avatar.jpg'};
+      image: 'avatar.jpg',
+      birthDate: '25/02/1998',
+      address: '23 Avenue, Paris, France'};
     this.initForm();
   }
 
@@ -44,7 +54,13 @@ export class ProfileFeatureComponent implements OnInit {
       lastName: [this.profile.lastName],
       email: [this.profile.email],
       phoneNumber: [this.profile.phoneNumber],
-      aboutYou: [this.profile.aboutYou]
+      aboutYou: [this.profile.aboutYou],
+      birthDate: [this.profile.birthDate],
+      address: [this.profile.address]
+    })
+
+    this.form.valueChanges.subscribe(() => {
+      this.buttonDisabled = false;
     })
   }
 
@@ -54,5 +70,13 @@ export class ProfileFeatureComponent implements OnInit {
 
   deleteProfileImage(): void{
     this.profile.image = '';
+  }
+
+  cancel(){
+    this.router.navigate(['/dashboard']);
+  }
+
+  save(){
+    this.customMessageService.showSuccess('Profile updated successfully!');
   }
 }
